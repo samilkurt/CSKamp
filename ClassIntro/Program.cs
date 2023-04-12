f38d04a526acafe597042797653005a1a61f238e
samil kurt <sam.361@hotmail.com>
2023-04-13 02:32:06 +0300

    Güncelleme

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Ali";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Şamil Kurt";
+            kurs1.IzlenmeOrani = 489;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Şamil Kurt";
+            kurs2.IzlenmeOrani = 324;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "C";
+            kurs3.Egitmen = "Şamil Kurt";
+            kurs3.IzlenmeOrani = 873;
+
+            Kurs[] Kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (Kurs kurs in Kurslar)
+            {
+                Console.WriteLine("Kurs adı: " + kurs.KursAdi+" " + "Eğitmen: " + kurs.Egitmen +" " + "İzlenme Oranı: " + kurs.IzlenmeOrani);
+            }
+        }
+    }
+    class Kurs 
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ DegerVeReferansTipler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("DegerVeReferansTipler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DegerVeReferansTipler")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("9acc1035-9907-4e08-abfe-17bb230d8745")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
